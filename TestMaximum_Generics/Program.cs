358fcc304a1f1f6fc11c78491945ea59db693766
Anant1202 <anantsingh1202@gmail.com>
2022-05-22 16:08:54 +0530

    Welcome to Test Maximum Program

---
+++ TestMaximum_Generics/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestMaximum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Test Maximum Program");
+        }
+
+    }
+}
+
