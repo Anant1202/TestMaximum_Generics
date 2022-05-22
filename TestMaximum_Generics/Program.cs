0e272b23a3ba05fe9c18d8b132a342adb51f7114
Anant1202 <anantsingh1202@gmail.com>
2022-05-22 17:17:44 +0530

    Added:String Maximum Value

--- TestMaximum_Generics/Program.cs
+++ TestMaximum_Generics/Program.cs
@@ -8,8 +8,9 @@ namespace TestMaximum_Generics
         {
             Console.WriteLine("Welcome to Test Maximum Program");
             FindMax ob1 = new FindMax();
-            Console.WriteLine("Maximum Integer Number is : "+ob1.IntMax(5, 8, 2));
+            Console.WriteLine("Maximum Integer Number is : " + ob1.IntMax(5, 8, 2));
             Console.WriteLine("Maximum Float Number is :" + ob1.FloatMax(3.25f, 5.67f, 8.12f));
+            Console.WriteLine("Maximum String Value is :" + ob1.StringMax("Apple","Peach","Banana"));
         }
 
     }
