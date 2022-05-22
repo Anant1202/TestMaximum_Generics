95c43dd2be09b77daf4d1a72fb568886ed7d63d5
Anant1202 <anantsingh1202@gmail.com>
2022-05-22 16:45:24 +0530

    Added:Integer Maximum value

--- TestMaximum_Generics/Program.cs
+++ TestMaximum_Generics/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 
-namespace TestMaximum
+namespace TestMaximum_Generics
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Test Maximum Program");
+            FindMax ob1 = new FindMax();
+            Console.WriteLine("Maximum number is : "+ob1.IntMax(5, 8, 2));
         }
 
     }
