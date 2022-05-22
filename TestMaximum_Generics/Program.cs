028ae1c82f2f38e228b2e36164da482d8803bd99
Anant1202 <anantsingh1202@gmail.com>
2022-05-22 18:23:44 +0530

    Added:Refactor1-Generic Method

--- TestMaximum_Generics/Program.cs
+++ TestMaximum_Generics/Program.cs
@@ -7,10 +7,14 @@ namespace TestMaximum_Generics
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Test Maximum Program");
-            FindMax ob1 = new FindMax();
-            Console.WriteLine("Maximum Integer Number is : " + ob1.IntMax(5, 8, 2));
-            Console.WriteLine("Maximum Float Number is :" + ob1.FloatMax(3.25f, 5.67f, 8.12f));
-            Console.WriteLine("Maximum String Value is :" + ob1.StringMax("Apple","Peach","Banana"));
+            //FindMax ob1 = new FindMax();
+            //Console.WriteLine("Maximum Integer Number is : " + ob1.IntMax(5, 8, 2));
+            //Console.WriteLine("Maximum Float Number is :" + ob1.FloatMax(3.25f, 5.67f, 8.12f));
+            //Console.WriteLine("Maximum String Value is :" + ob1.StringMax("Apple","Peach","Banana"));
+            GenericsMax ob2 = new GenericsMax();
+            Console.WriteLine("Maximum Integer Number is : " + ob2.Generic<int>(5, 8, 2));
+            Console.WriteLine("Maximum Float Number is :" + ob2.Generic<float>(3.25f, 5.67f, 8.12f));
+            Console.WriteLine("Maximum String Value is :" + ob2.Generic<string>("Apple", "Peach", "Banana"));
         }
 
     }
